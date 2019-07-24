505a78143aa877be0c5b43068eee82c47f4b09c8
Kevin Alwell <alwell-kevin@github.com>
2019-07-24 12:02:16 -0400

    Initial Commit; Learn-teleport

---
+++ Wizard.js
@@ -0,0 +1,36 @@
+class Wizard {
+// Wizard should cast spells. 
+  constructor() {
+    this.magicReady = false;
+    this.location = "The Shire"
+  }
+
+  warmUp(spell) {
+    if(spell){
+      this.magicReady = true;
+      this.spell = spell;
+    }
+
+    return
+  }
+
+  castSpell(){
+    if(this.magicReady){
+      console.log("WIZARD CASTED: ", this.spell);
+
+      return this.spell;
+    }
+
+    return
+  }
+
+  teleport(destination){
+      console.log("WIZARD TELEPORTING TO ", destination);
+      this.location = destination
+    
+      return
+  }
+
+}
+
+module.exports = Wizard;
